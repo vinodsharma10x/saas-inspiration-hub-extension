0b506b8c58e43ece34ffbbca989f903768fbbab6
Vinod Sharma <122154785+vinodsharma10x@users.noreply.github.com>
2024-09-26 05:11:40 -0400

    updated the manifest file for correct description.

--- script.js
+++
@@ -1,9 +0,0 @@
-// Smooth scrolling for navigation links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
